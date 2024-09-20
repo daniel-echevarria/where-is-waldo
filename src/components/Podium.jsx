cd09e9ee185514954d9438f28f78f00228307f97
Kasper Bak <102696643+SpookyKasper@users.noreply.github.com>
2024-09-20 22:27:20 +0200

    Display top scores to user

--- src/components/Podium.jsx
+++ src/components/Podium.jsx
@@ -1,15 +1,25 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 
 const Podium = () => {
+  const [topScores, setTopScores] = useState([]);
   useEffect(() => {
     const getTopScores = async () => {
       const response = await fetch("http://localhost:3000/scores_top");
       const result = await response.json();
-      console.log(result);
+      setTopScores(result);
     };
     getTopScores();
   }, []);
-  return <div className="podium"></div>;
+
+  const ScoreList = topScores.map((topScore) => {
+    return (
+      <div key={topScore.id} className="score-row">
+        <span className="name">{topScore.name}</span>
+        <span className="timeScore">{topScore.time_score}</span>
+      </div>
+    );
+  });
+  return <div className="podium">{ScoreList}</div>;
 };
 
 export default Podium;
