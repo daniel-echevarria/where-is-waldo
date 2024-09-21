813e9fd12b3c8d868fad3e60d46f1999f64f6756
Kasper Bak <102696643+SpookyKasper@users.noreply.github.com>
2024-09-21 12:23:33 +0200

    Add colors to podium

--- src/components/Podium.jsx
+++ src/components/Podium.jsx
@@ -1,15 +1,24 @@
 import { useEffect, useState } from "react";
+import "./Podium.css";
 
 const Podium = ({ topScores }) => {
-  const ScoreList = topScores.map((topScore) => {
+  const colors = ["gold", "silver", "bronze"];
+
+  const ScoreList = topScores.map((topScore, index) => {
     return (
-      <div key={topScore.id} className="score-row">
+      <div key={topScore.id} className={`score-row ${colors[index]} `}>
+        <span>{index + 1}.</span>
         <span className="name">{topScore.name}</span>
-        <span className="timeScore">{topScore.time_score}</span>
+        <span className="timeScore">{topScore.time_score}s</span>
       </div>
     );
   });
-  return <div className="podium">{ScoreList}</div>;
+  return (
+    <div className="podium">
+      <span>FASTEST WALLYERS</span>
+      {ScoreList}
+    </div>
+  );
 };
 
 export default Podium;
