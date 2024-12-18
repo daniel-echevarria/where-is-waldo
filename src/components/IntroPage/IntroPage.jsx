8ab22b1a55e06de2a120629fcf7d1599ddb1c0f7
Kasper Bak <dan.echevarri@gmail.com>
2024-12-18 11:22:24 +0100

    Fix answer-box display and update fonts

--- src/components/IntroPage/IntroPage.jsx
+++ src/components/IntroPage/IntroPage.jsx
@@ -4,8 +4,8 @@ const IntroPage = ({ setIntroOver }) => {
   return (
     <div className="intro">
       <h1>Where the F*ck is Waldo ?</h1>
-      <h2>find the targets as fast as possible... good luck!</h2>
-      <p>can you make it to the grand podium ?</p>
+      <h2>Find the targets as fast as possible... good luck!</h2>
+      <p>Can you make it to the grand podium ?</p>
       <button className="start-btn" onClick={() => setIntroOver(true)}>
         Start Game
       </button>
