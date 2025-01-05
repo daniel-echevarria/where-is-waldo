b8dfe574667734814191ba83771c8744837471d3
Kasper Bak <dan.echevarri@gmail.com>
2025-01-05 12:57:41 +0100

    Change homepage title for a more professional tone

--- src/components/IntroPage/IntroPage.jsx
+++ src/components/IntroPage/IntroPage.jsx
@@ -3,7 +3,7 @@ import "./IntroPage.css";
 const IntroPage = ({ setIntroOver }) => {
   return (
     <div className="intro">
-      <h1>Where the F*ck is Waldo ?</h1>
+      <h1>Where the Heck is Waldo ?</h1>
       <h2>Find the targets as fast as possible... good luck!</h2>
       <p>Can you make it to the grand podium ?</p>
       <button className="start-btn" onClick={() => setIntroOver(true)}>
